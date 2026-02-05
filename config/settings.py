6a01c2b165efadff65f3f56319fdacc2b444bbd3
Elektrokot <markus.rulit@gmail.com>
2026-02-05 23:35:12 +1000

    test ci 11

--- config/settings.py
+++ config/settings.py
@@ -14,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.getenv("DEBUG") == "True" else False
+DEBUG = True if os.getenv("DJANGO_DEBUG") == "True" else False
 
 ALLOWED_HOSTS = ["*"]
 
